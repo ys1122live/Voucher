6c50895df53dd2443f4cb5d2f1838d47b1621cd5
ys1122live <97042262+ys1122live@users.noreply.github.com>
2022-05-21 19:51:22 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,3 +10,22 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
+mkdir /home/ubuntu/app
+mkdir /home/ubuntu/docker
+mkdir /home/ubuntu/mysql
+mkdir /home/ubuntu/mysql/data
+mkdir /home/ubuntu/mysql/mysql-files
+
+sudo docker pull mysql:8.0.27
+sudo docker pull phpmyadmin:latest
+
+curl https://raw.githubusercontent.com/ys1122live/Voucher/main/Dockerfile -o /home/ubuntu/docker/Dockerfile
+sudo docker build -t dotnetcore -f /home/ubuntu/docker/Dockerfile .
+
+curl https://raw.githubusercontent.com/ys1122live/Voucher/main/GameVoucherManageCore.zip -o /home/ubuntu/app/GameVoucherManageCore.zip
+unzip /home/ubuntu/app/GameVoucherManageCore.zip -d /home/ubuntu/app
+rm /home/ubuntu/app/GameVoucherManageCore.zip
+
+sudo docker run --name mysql --restart always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=yansong -e MYSQL_ROOT_HOST=% -e TZ="Asia/Shanghai" -v /home/ubuntu/mysql/data:/var/lib/mysql -v /home/ubuntu/mysql/mysql-files:/var/lib/mysql-files -v /home/ubuntu/mysql/conf.d:/etc/mysql/conf.d -d mysql:8.0.27
+sudo docker run --name phpmyadmin -d -e PMA_ARBITRARY=1 -p 5200:80 phpmyadmin:latest
+sudo docker run --name dotnetcore --restart always -p 80:80 -d dotnetcore:latest
