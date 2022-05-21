5d722305f2b57079c365d63ff852845e58fa2945
ys1122live <97042262+ys1122live@users.noreply.github.com>
2022-05-22 00:14:05 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,6 @@ sudo apt update
 
 sudo apt upgrade -y
 
-#安装docker
 sudo apt install ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -26,6 +25,11 @@ curl https://raw.githubusercontent.com/ys1122live/Voucher/main/GameVoucherManage
 unzip /home/ubuntu/app/GameVoucherManageCore.zip -d /home/ubuntu/app
 rm /home/ubuntu/app/GameVoucherManageCore.zip
 
-sudo docker run --name mysql --restart always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=Qq600210. -e MYSQL_ROOT_HOST=172.17.0.% -e TZ="Asia/Shanghai" -v /home/ubuntu/mysql/data:/var/lib/mysql -v /home/ubuntu/mysql/mysql-files:/var/lib/mysql-files -v /home/ubuntu/mysql/conf.d:/etc/mysql/conf.d -d mysql:8.0.27
-sudo docker run --name phpmyadmin -d -e PMA_ARBITRARY=1 -p 9001:80 phpmyadmin:latest
-sudo docker run --name dotnetcore --restart always -p 80:80 -e TZ="Asia/Shanghai" -v /home/ubuntu/app:/app  -d dotnetcore:latest
+curl https://raw.githubusercontent.com/ys1122live/Voucher/main/mysql.zip -o /home/ubuntu/mysql/data.zip
+unzip /home/ubuntu/mysql/data.zip -d /home/ubuntu/mysql/data
+rm /home/ubuntu/mysql/data.zip
+
+sudo docker run --name mysql --restart always -d  -p 3306:3306 -e TZ="Asia/Shanghai" -v /home/ubuntu/mysql/data:/var/lib/mysql -v /home/ubuntu/mysql/mysql-files:/var/lib/mysql-files -v /home/ubuntu/mysql/conf.d:/etc/mysql/conf.d mysql:8.0.27
+sudo docker run --name dotnetcore --restart always -d -p 80:80 -e TZ="Asia/Shanghai" -v /home/ubuntu/app:/app dotnetcore:latest
+sudo docker run --name phpmyadmin --restart no -d -e PMA_HOST=172.17.0.2 -p 9001:80 phpmyadmin:latest
+sudo docker run --name portainer --restart no -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce
