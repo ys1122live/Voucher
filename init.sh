07bbb3806f031a0f94202d1f884d71815a15505b
ys1122live <97042262+ys1122live@users.noreply.github.com>
2022-05-21 19:58:15 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -28,4 +28,4 @@ rm /home/ubuntu/app/GameVoucherManageCore.zip
 
 sudo docker run --name mysql --restart always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=yansong -e MYSQL_ROOT_HOST=% -e TZ="Asia/Shanghai" -v /home/ubuntu/mysql/data:/var/lib/mysql -v /home/ubuntu/mysql/mysql-files:/var/lib/mysql-files -v /home/ubuntu/mysql/conf.d:/etc/mysql/conf.d -d mysql:8.0.27
 sudo docker run --name phpmyadmin -d -e PMA_ARBITRARY=1 -p 5200:80 phpmyadmin:latest
-sudo docker run --name dotnetcore --restart always -p 80:80 -d dotnetcore:latest
+sudo docker run --name dotnetcore --restart always -p 80:80 -e TZ="Asia/Shanghai" -v /home/ubuntu/app:/app  -d dotnetcore:latest
