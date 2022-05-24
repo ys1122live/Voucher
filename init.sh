94978e049f669c14626e2621fd12d31448a413ae
ys1122live <97042262+ys1122live@users.noreply.github.com>
2022-05-24 16:53:14 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -14,6 +14,7 @@ mkdir /home/ubuntu/docker
 mkdir /home/ubuntu/mysql
 mkdir /home/ubuntu/mysql/data
 mkdir /home/ubuntu/mysql/mysql-files
+mkdir /home/ubuntu/portainer
 
 sudo docker pull mysql:8.0.27
 sudo docker pull phpmyadmin:latest
@@ -21,6 +22,10 @@ sudo docker pull portainer/portainer-ce:latest
 
 curl https://raw.githubusercontent.com/ys1122live/Voucher/main/Dockerfile -o /home/ubuntu/docker/Dockerfile
 sudo docker build -t dotnetcore -f /home/ubuntu/docker/Dockerfile .
+
+curl https://raw.githubusercontent.com/ys1122live/Voucher/main/portainer.zip -o /home/ubuntu/portainer.zip
+unzip /home/ubuntu/portainer.zip -d /home/ubuntu/portainer
+rm /home/ubuntu/portainer.zip
 
 curl https://raw.githubusercontent.com/ys1122live/Voucher/main/GameVoucherManageCore.zip -o /home/ubuntu/GameVoucherManageCore.zip
 unzip /home/ubuntu/GameVoucherManageCore.zip -d /home/ubuntu/GameVoucherManageCore
@@ -33,4 +38,4 @@ rm /home/ubuntu/mysql.zip
 sudo docker run --name mysql --restart always -d -p 3306:3306 -e TZ="Asia/Shanghai" -v /home/ubuntu/mysql/data:/var/lib/mysql -v /home/ubuntu/mysql/mysql-files:/var/lib/mysql-files -v /home/ubuntu/mysql/conf.d:/etc/mysql/conf.d mysql:8.0.27
 sudo docker run --name GameVoucherManageCore --restart always -d -p 80:80 -e TZ="Asia/Shanghai" -v /home/ubuntu/GameVoucherManageCore:/app --cgroupns host dotnetcore:latest
 sudo docker run --name phpmyadmin --restart no -d -e PMA_HOST=172.17.0.2 -p 9001:80 phpmyadmin:latest
-sudo docker run --name portainer --restart no -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce
+sudo docker run --name portainer --restart no -d -p 9000:9000 -v /home/ubuntu/portainer:/data -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce
