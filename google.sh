d63d02eb0ca9501a27abe70001ce3deb9db08b76
ys1122live <97042262+ys1122live@users.noreply.github.com>
2022-05-24 18:12:05 +0800

    Update google.sh

--- google.sh
+++ google.sh
@@ -35,7 +35,8 @@ curl https://raw.githubusercontent.com/ys1122live/Voucher/main/mysql.zip -o /hom
 unzip /home/ubuntu/mysql.zip -d /home/ubuntu/mysql/data
 rm /home/ubuntu/mysql.zip
 
-sudo docker run --name mysql --restart always -d -p 3306:3306 -ip 172.17.0.2 -e TZ="Asia/Shanghai" -v /home/ubuntu/mysql/data:/var/lib/mysql -v /home/ubuntu/mysql/mysql-files:/var/lib/mysql-files -v /home/ubuntu/mysql/conf.d:/etc/mysql/conf.d mysql:8.0.27
-sudo docker run --name GameVoucherManageCore --restart always -d -p 80:80 -ip 172.17.0.3 -e TZ="Asia/Shanghai" -v /home/ubuntu/GameVoucherManageCore:/app --cgroupns host dotnetcore:latest
-sudo docker run --name phpmyadmin --restart no -d -p 9001:80 -ip 172.17.0.10 -e PMA_HOST=172.17.0.2 phpmyadmin:latest
-sudo docker run --name portainer --restart no -d -p 9000:9000 -ip 172.17.0.11 -v /home/ubuntu/portainer:/data -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce
+sudo docker network create --subnet=172.18.0.0/24 network
+sudo docker run --name mysql --restart always -d -p 3306:3306 --network network --ip 172.18.0.2 -e TZ="Asia/Shanghai" -v /home/ubuntu/mysql/data:/var/lib/mysql -v /home/ubuntu/mysql/mysql-files:/var/lib/mysql-files -v /home/ubuntu/mysql/conf.d:/etc/mysql/conf.d mysql:8.0.27
+sudo docker run --name GameVoucherManageCore --restart always -d -p 80:80 --network network --ip 172.18.0.3 -e TZ="Asia/Shanghai" -v /home/ubuntu/GameVoucherManageCore:/app --cgroupns host dotnetcore:latest
+sudo docker run --name phpmyadmin --restart no -d -p 9001:80 --network network --ip 172.18.0.10 -e PMA_HOST=172.17.0.2 phpmyadmin:latest
+sudo docker run --name portainer --restart no -d -p 9000:9000 --network network --ip 172.18.0.11 -v /home/ubuntu/portainer:/data -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce
