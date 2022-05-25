82832972d4a5b7b681ad4da8131ed591754ef925
ys1122live <97042262+ys1122live@users.noreply.github.com>
2022-05-26 00:01:16 +0800

    Update google.sh

--- google.sh
+++ google.sh
@@ -2,7 +2,7 @@ sudo apt update
 
 sudo apt upgrade -y
 
-sudo apt install ca-certificates curl gnupg lsb-release -y
+sudo apt install ca-certificates curl gnupg lsb-release unzip -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
