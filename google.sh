ea882d831b25c305d7e687e7895d9eec4ba8dd73
ys1122live <97042262+ys1122live@users.noreply.github.com>
2022-05-26 11:47:46 +0800

    Update google.sh

--- google.sh
+++ google.sh
@@ -1,3 +1,13 @@
+if [ -z "$1" ] ;then
+	echo "mysql root pass empty"
+	exit
+fi
+
+if [ -z "$2" ] ;then
+	echo "mysql database pass empty"
+	exit
+fi
+
 sudo apt update
 
 sudo apt upgrade -y
@@ -31,11 +41,28 @@ sudo curl https://raw.githubusercontent.com/ys1122live/Voucher/main/Google.zip -
 sudo unzip /mnt/GameVoucherManageCore.zip -d /mnt/GameVoucherManageCore
 sudo rm /mnt/GameVoucherManageCore.zip
 
-sudo curl https://raw.githubusercontent.com/ys1122live/Voucher/main/google_mysql.zip -o /mnt/mysql.zip
-sudo unzip /mnt/mysql.zip -d /mnt/mysql/data
-sudo rm /mnt/mysql.zip
+sudo tee /mnt/GameVoucherManageCore/appsettings.json << EOF
+{
+	"ConnectionStrings": {
+		"type": "mysql",
+		"version": "8.0.27-mysql",
+		"connection": "server=172.18.0.2;uid=GameVoucherManage;pwd=$2;database=GameVoucherManage;"
+	},
+	"AllowedHosts": "*"
+}
+EOF
 
 sudo docker network create --subnet=172.18.0.0/24 network
+
+sudo docker run --name mysql --restart always -d -p 3306:3306 --network network --ip 172.18.0.2 -e TZ="Asia/Shanghai" -e MYSQL_ROOT_PASSWORD=$1 -e MYSQL_DATABASE=GameVoucherManage -e MYSQL_USER=GameVoucherManage -e MYSQL_PASSWORD=$2 -v /mnt/mysql/data:/var/lib/mysql -v /mnt/mysql/mysql-files:/var/lib/mysql-files -v /mnt/mysql/conf.d:/etc/mysql/conf.d mysql:8.0.27
+sleep 20s
+sudo curl https://raw.githubusercontent.com/ys1122live/Voucher/main/GameVoucherManage.sql -o /mnt/GameVoucherManage.sql
+sudo docker exec -i mysql sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" GameVoucherManage' < /mnt/GameVoucherManage.sql
+sudo rm /mnt/GameVoucherManage.sql
+sleep 10s
+sudo docker stop mysql
+sudo docker rm mysql
+
 sudo docker run --name mysql --restart always -d -p 3306:3306 --network network --ip 172.18.0.2 -e TZ="Asia/Shanghai" -v /mnt/mysql/data:/var/lib/mysql -v /mnt/mysql/mysql-files:/var/lib/mysql-files -v /mnt/mysql/conf.d:/etc/mysql/conf.d mysql:8.0.27
 sudo docker run --name GameVoucherManageCore --restart always -d -p 80:80 --network network --ip 172.18.0.3 -e TZ="Asia/Shanghai" -v /mnt/GameVoucherManageCore:/app --cgroupns host dotnetcore:latest
 sudo docker run --name phpmyadmin --restart no -d -p 9001:80 --network network --ip 172.18.0.10 -e PMA_HOST=172.18.0.2 phpmyadmin:latest
