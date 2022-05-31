b8c9f529101f6c0c1631a5971ba9c8b9d63c0768
ys1122live <97042262+ys1122live@users.noreply.github.com>
2022-05-31 11:22:28 +0800

    Update google.sh

--- google.sh
+++ google.sh
@@ -38,10 +38,6 @@ EOF
 sudo docker build -t dotnetcore -f /home/$USER/Dockerfile .
 sudo rm /home/$USER/Dockerfile
 
-sudo curl https://raw.githubusercontent.com/ys1122live/Voucher/main/portainer.zip -o /home/$USER/portainer.zip
-sudo unzip /home/$USER/portainer.zip -d /home/$USER/portainer
-sudo rm /home/$USER/portainer.zip
-
 sudo curl https://raw.githubusercontent.com/ys1122live/Voucher/main/Google.zip -o /home/$USER/GameVoucherManage.zip
 sudo unzip /home/$USER/GameVoucherManage.zip -d /home/$USER/GameVoucherManage
 sudo rm /home/$USER/GameVoucherManage.zip
@@ -72,3 +68,5 @@ sudo docker run --name mysql --restart always -d -p 3306:3306 --network network 
 sudo docker run --name GameVoucherManage --restart always -d -p 80:80 --network network --ip 172.18.0.3 -e TZ="Asia/Shanghai" -v /home/$USER/GameVoucherManage:/app --cgroupns host dotnetcore:latest
 sudo docker run --name phpmyadmin --restart no -d -p 9001:80 --network network --ip 172.18.0.10 -e PMA_HOST=172.18.0.2 phpmyadmin:latest
 sudo docker run --name portainer --restart no -d -p 9000:9000 --network network --ip 172.18.0.11 -v /home/$USER/portainer:/data -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce
+
+curl 'http://172.18.0.11:9000/api/users/admin/init' -H 'Content-Type: application/json' --data-raw '{"Username":"admin","Password":"ys1122@live.cn"}'
