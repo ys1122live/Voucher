2a7acf65d615299cb6d1a1c303755677a7c2b20d
ys1122live <97042262+ys1122live@users.noreply.github.com>
2022-06-19 22:41:09 +0800

    Create frps.sh

---
+++ frps.sh
@@ -0,0 +1,34 @@
+mkdir -p ~/docker
+
+tee ~/docker/Dockerfile << EOF
+FROM alpine:latest
+RUN apk update
+WORKDIR /app
+ENTRYPOINT ["/app/frps", "-c", "frps.ini"]
+EOF
+
+docker build -t frps -f ~/docker/Dockerfile .
+rm ~/docker/Dockerfile
+
+if [ $(uname -m) = "x86_64" ]; then
+    PLATFORM=amd64
+fi
+
+if [ $(uname -m) = "aarch64" ]; then
+    PLATFORM=arm64
+fi
+
+FRP_VERSION=0.43.0
+FILE_NAME=frp_${FRP_VERSION}_linux_${PLATFORM}
+
+wget -P https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ~/${FILE_NAME}.tar.gz
+
+tar -zxvf ~/${FILE_NAME}.tar.gz -C ~/
+mv ~/${FILE_NAME} ~/frp
+
+tee ~/frp/frps.ini << EOF
+[common]
+bind_port = 7000
+EOF
+
+docker run --name frps --restart always -d --network network --ip 172.18.0.20 -p 7000:7000 -p 8898:8898 -e TZ="Asia/Shanghai" -v ~/frp:/app frps:latest
