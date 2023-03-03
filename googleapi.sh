63b7dad6cf96864de18a0e5492eaa36a53f897b3
ys1122live <97042262+ys1122live@users.noreply.github.com>
2023-03-04 01:06:48 +0800

    Update googleapi.sh

--- googleapi.sh
+++ googleapi.sh
@@ -44,3 +44,6 @@ EOF
 sudo docker network create --subnet=172.18.0.0/24 network
 sudo docker run --name GameVoucherApi --restart always -d -p 80:80 --network network --ip 172.18.0.3 -e TZ="Asia/Shanghai" -v ~/GameVoucherApi:/app -w /app --cgroupns host mcr.microsoft.com/dotnet/aspnet:6.0 ./GameVoucherApiCore
 sudo docker run --name portainer --restart no -d -p 9000:9000 --network network --ip 172.18.0.11 -v ~/portainer:/data -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce
+
+sleep 20s
+curl 'http://127.0.0.1:9000/api/users/admin/init' -H 'Content-Type: application/json' --data-raw '{"Username":"admin","Password":"ys1122@live.cn"}'
